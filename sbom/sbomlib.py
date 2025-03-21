9840b12ebc648b59ed36a567e86e3050d98b6835
Martin Jediny <jedinym@proton.me>
2025-03-21 19:22:54 +0100

    feat(ISV-5760): add sbom library
    Signed-off-by: Martin Jediny <jedinym@proton.me>

---
+++ sbom/sbomlib.py
@@ -0,0 +1,41 @@
+import json
+from typing import IO
+from pathlib import Path
+
+
+def get_oci_auth_file(reference: str, auth: Path, fp: IO) -> bool:
+    """
+    Gets path to a temporary file containing the docker config JSON for <reference>.
+
+    Args:
+        reference (str): Reference to an image in the form registry/repo@sha256-deadbeef
+        auth (Path): Existing docker config.json
+        fp (IO): File object to write the new auth file to
+    """
+    if not auth.is_file():
+        raise ValueError(f"No docker config file at {auth}")
+
+    # Remove digest (e.g. @sha256:...)
+    ref = reference.split("@", 1)[0]
+
+    # Registry is up to the first slash
+    registry = ref.split("/", 1)[0]
+
+    with open(auth, "r") as f:
+        config = json.load(f)
+    auths = config.get("auths", {})
+
+    current_ref = ref
+
+    while True:
+        token = auths.get(current_ref)
+        if token is not None:
+            json.dump({"auths": {registry: token}}, fp)
+            return True
+
+        if "/" not in current_ref:
+            break
+        current_ref = current_ref.rsplit("/", 1)[0]
+
+    json.dump({"auths": {}}, fp)
+    return False
