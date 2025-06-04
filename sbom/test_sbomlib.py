8a4eb6edbbc07c991478f0485922c8aedb60ffc2
Martin Jediny <jedinym@proton.me>
2025-06-04 15:24:09 +0200

    fixup! fix(ISV-5990): make naming clearer
    Signed-off-by: Martin Jediny <jedinym@proton.me>

--- sbom/test_sbomlib.py
+++ sbom/test_sbomlib.py
@@ -37,7 +37,7 @@ def test_get_oci_auth_file(auths, reference, expected_auths):
         json.dump(test_config, config)
         config.flush()
 
-        with sbomlib.make_oci_auth_file(reference, auth=Path(config.name)) as authfile:
+        with sbomlib.make_oci_auth_file(reference, authfile=Path(config.name)) as authfile:
             with open(authfile, "r") as fp:
                 data = json.loads(fp.read())
                 assert data["auths"] == expected_auths
